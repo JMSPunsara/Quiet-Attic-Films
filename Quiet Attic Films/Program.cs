9243f4b5d199d41d5a990aa428970188cca54c4d
Punsara JMS <E154631@esoft.academy>
2024-01-23 18:40:34 +0530

    Add project files.

---
+++ Quiet Attic Films/Program.cs
@@ -0,0 +1,17 @@
+namespace Quiet_Attic_Films
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Progress_Bar());
+        }
+    }
+}
---
+++ Quiet Attic Films/Progress_Bar.cs
@@ -0,0 +1,10 @@
+namespace Quiet_Attic_Films
+{
+    public partial class Progress_Bar : Form
+    {
+        public Progress_Bar()
+        {
+            InitializeComponent();
+        }
+    }
+}
